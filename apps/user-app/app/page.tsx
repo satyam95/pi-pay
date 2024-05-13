5832911aa4f7504e50c111179541e8a7b18018dd
Satyam Sagar <satyamsagar30@gmail.com>
2024-05-13 16:39:26 +0530

    feat: user app ui

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,19 +1,13 @@
-"use client";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Appbar } from "@repo/ui/appbar";
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "./lib/auth";
 
-
-export default function Page(): JSX.Element {
-  const session = useSession();
-  return (
-    <main>
-      <div>
-        <Appbar
-          onSignin={signIn}
-          onSignout={signOut}
-          user={session.data?.user}
-        />
-      </div>
-    </main>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
+  
 }
