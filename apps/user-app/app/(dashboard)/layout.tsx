d279130f8419e4d08d7b0ee836fd561266a0e64a
Satyam Sagar <satyamsagar30@gmail.com>
2024-05-27 19:15:19 +0530

    feat: add p2p transaction

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -20,10 +20,34 @@ export default function Layout({
             icon={<TransactionsIcon />}
             title="Transactions"
           />
+          <SidebarItem
+            href={"/p2p"}
+            icon={<P2PTransferIcon />}
+            title="P2P Transfer"
+          />
         </div>
       </div>
       {children}
     </div>
+  );
+}
+
+function P2PTransferIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+      />
+    </svg>
   );
 }
 
--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,3 +1,51 @@
-export default function () {
-  return <div>Transactions</div>;
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import prisma from "@repo/db/client";
+import { OnRampTransactions } from "../../../components/OnRampTransaction";
+import { Card } from "@repo/ui/card";
+
+async function getp2pTransactions() {
+  const session = await getServerSession(authOptions);
+  const txns = await prisma.p2pTransfer.findMany({
+    where: {
+      fromUserId: Number(session?.user?.id),
+    },
+    orderBy: { timestamp: "desc" },
+  });
+  return txns.map((tx) => ({
+    time: tx.timestamp,
+    amount: tx.amount,
+    fromUserId: tx.fromUserId,
+    toUserId: tx.toUserId,
+  }));
+}
+
+export default async function () {
+  const transactions = await getp2pTransactions();
+  return (
+    <div className="w-full h-full">
+      <div className="text-2xl text-[#6a51a6] pt-6 pl-5 mb-4 font-bold subpixel-antialiased">
+        Transactions
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 p-5">
+        <Card title="Recent Transactions">
+          <div className="pt-2">
+            {transactions.map((t, idx) => (
+              <div key={idx} className="flex justify-between">
+                <div>
+                  <div className="text-sm">Send INR</div>
+                  <div className="text-slate-600 text-xs">
+                    {t.time.toDateString()}
+                  </div>
+                </div>
+                <div className="flex flex-col justify-center">
+                  + Rs {t.amount / 100}
+                </div>
+              </div>
+            ))}
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 }
